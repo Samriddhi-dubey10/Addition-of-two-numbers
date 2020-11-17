176146ddbe6adc33a9c8eb54505307ce037413af
Samriddhi-dubey10 <74581936+Samriddhi-dubey10@users.noreply.github.com>
2020-11-17 09:53:46 +0530

    Add files via upload

---
+++ Add two numbers/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+    cout<<"enter two no";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"sum of two no is "<<sum<<"\n";
+    return 0;
+}
